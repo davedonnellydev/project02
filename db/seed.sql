47ed70f7c6829526b00b3d984ddcd6ba3d24d3cb
David Buckley <davidpaulbuckley@gmail.com>
2022-08-02 20:54:58 +1000

    Update requirements & seed file

--- db/seed.sql
+++ db/seed.sql
@@ -3,4 +3,5 @@ TRUNCATE lists restart identity cascade;
 TRUNCATE movies restart identity cascade;
 
 INSERT INTO users(username,email,fname,lname,password) VALUES
-('daveb','dave@ga.com','Dave','Buckley','$2b$12$GWwOl7kc3eAQW6gBa39LNOw11uIPm.4a2ex3BLTJ0ysu41aBZDYs6');
+('daveb','dave@ga.com','Dave','Buckley','$2b$12$GWwOl7kc3eAQW6gBa39LNOw11uIPm.4a2ex3BLTJ0ysu41aBZDYs6'),
+('Chrispy','chris@ga.com','Chris','Smith','$2b$12$2u/y2NryA.QnbjOeJuA7b.yk8kSkHvdgv3FBPfhE9n7gTp/14rn.O');
